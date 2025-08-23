523cc2e85933021ac78d916b4aba63916f5919af
mtanng9 <mlt152498@gmail.com>
2025-08-23 11:43:46 -0800

    added routes for about, login, and signup

--- internal/server/server.go
+++ internal/server/server.go
@@ -22,6 +22,9 @@ func StartServer() {
 	mux.HandleFunc("GET /settings", handleSettingsPage)
 	mux.HandleFunc("GET /favorites", handleFavoritePage)
 	mux.HandleFunc("GET /chatlog", handleChatLog)
+	mux.HandleFunc("GET /about", handleAbout)
+	mux.HandleFunc("GET /login", handleLogIn)
+	mux.HandleFunc("GET /signup", handleSignUp)
 
 	fmt.Printf("Starting Server on PORT: %d", PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux))
@@ -60,5 +63,23 @@ func handleFavoritePage(w http.ResponseWriter, r *http.Request) {
 func handleChatLog(w http.ResponseWriter, r *http.Request) {
 	page := pages.ChatLog()
 	layout := layouts.Main("Chat Log", page)
+	layout.Render(context.TODO(), w)
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+	page := pages.About()
+	layout := layouts.Main("About", page)
+	layout.Render(context.TODO(), w)
+}
+
+func handleLogIn(w http.ResponseWriter, r *http.Request) {
+	page := pages.LogIn()
+	layout := layouts.Main("Log In", page)
+	layout.Render(context.TODO(), w)
+}
+
+func handleSignUp(w http.ResponseWriter, r *http.Request) {
+	page := pages.SignUp()
+	layout := layouts.Main("Sign Up", page)
 	layout.Render(context.TODO(), w)
 }
